2bfb78941b118af82b709d638276294638dfbbd5
tkgstrator <nasawake.am@gmail.com>
2022-03-24 11:25:23 +0900

    - Change port, enabled playground, debug

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,14 +3,16 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { GraphQLModule } from '@nestjs/graphql';
 import { join } from 'path';
-import { ApolloDriver } from '@nestjs/apollo';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [
-    GraphQLModule.forRoot({
+    GraphQLModule.forRoot<ApolloDriverConfig>({
       autoSchemaFile: true,
       driver: ApolloDriver,
+      playground: true,
+      debug: true,
     }),
     UsersModule,
   ],
