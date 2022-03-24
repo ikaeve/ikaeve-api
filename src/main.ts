bf5461f77d810f0fb47031e593074352c656cb7e
tkgstrator <nasawake.am@gmail.com>
2022-03-24 11:35:24 +0900

    - Add FastifyAdaptor

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import {
+  FastifyAdapter,
+  NestFastifyApplication,
+} from '@nestjs/platform-fastify';
+
+import { ApolloServer } from 'apollo-server-fastify';
+import { ApolloServerPluginDrainHttpServer } from 'apollo-server-core';
+import { ApolloServerPlugin } from 'apollo-server-plugin-base';
+import fastify, { FastifyInstance } from 'fastify';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestFastifyApplication>(
+    AppModule,
+    new FastifyAdapter(),
+  );
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(5000);
 }
